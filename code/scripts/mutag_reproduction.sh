84bbe266bf4c606b25fdc1661c48c8f7a2d7aca5
xwieme <xander.wieme@ugent.be>
2023-12-01 13:15:55 +0100

    Correct mutagenicity hpc script, edit .gitignore

--- code/scripts/mutag_reproduction.sh
+++ code/scripts/mutag_reproduction.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
 #PBS -l nodes=1:ppn=2
 #PBS -l gpus=1
 #PBS -l walltime=02:00:00
+#PBS -l mem=16gb
 
 cd $VSC_SCRATCH
 # Make python module XAIChem globaly available
-export PYTHONPATH=code/XAIChem:$PYTHONPATH
+export PYTHONPATH=$VSC_SCRATCH/MastatThesis/code/XAIChem:$PYTHONPATH
+# Login to wandb to keep track of training progress
+source MastatThesis/code/env.sh
 
 # Train the GNN model in an apptainer container
-apptainer exec --nv /opt/conda/envs/lab/bin/python xai_chemistry_lab MastatThesis/code/scripts/mutage_reproduction.py --data_dir "MastatThesis/data"
+apptainer exec --nv xai_chemistry_lab.sif /opt/conda/envs/lab/bin/python MastatThesis/code/scripts/mutag_reproduction.py --data_dir "MastatThesis/data"
