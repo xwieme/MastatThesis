4b9085fe70d365b458638d2a0ac4811161b16ce1
xwieme <xander.wieme@ugent.be>
2024-03-27 20:34:42 +0100

    Manual rank check and compute fidelity for attribution methods

---
+++ code/scripts/esol/evaluation_metrics.py
@@ -0,0 +1,136 @@
+# Evaluation the different attribution methods using different metrics commonly
+# used to evaluate graph neural network explanations
+import os
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import torch
+import XAIChem
+from dash import Dash, dcc, html
+from rdkit import Chem
+
+DATA_DIR = "../../../data/ESOL"
+
+
+def fidelity(
+    group: pd.DataFrame,
+    attribution_method: str,
+    models: list,
+    device: str = "cpu",
+    mode: str = "max",
+) -> float:
+    """
+    Compute the difference between the model prediction and the prediction where
+    the most or least important features (i.e. the explanation) are masked. This
+    metric indicates if the explanation is faithfull to the model as removing the
+    most important features should significantly change the model output, whereas
+    remove the least important features should not result in a big output change.
+
+    :param group: pandas dataframe containing the substructures and attributions
+        of a molecule.
+    :param attribution_method: column name of the attributions used to select the
+        important substructures.
+    :param models: the graph neural network models which are explained.
+    :param device: which device to use for model prediction either 'cpu' or 'gpu'
+        (default is 'cpu')
+    :param mode: remove most important features ('max') or remove least important
+        features ('min') (default is 'max')
+    """
+
+    if mode == "max":
+        most_important_substructs = group.query(
+            f"{attribution_method}.abs() == {attribution_method}.abs().max()"
+        )
+    elif mode == "min":
+        most_important_substructs = group.query(
+            f"{attribution_method}.abs() == {attribution_method}.abs().min()"
+        )
+    else:
+        raise ValueError(
+            f"given mode '{mode}' is not supported, should be either 'min' or 'max'"
+        )
+
+    smiles = group.molecule_smiles.iloc[0]
+    rdmol = Chem.MolFromSmiles(smiles)
+
+    # All information necessary is available if only one substructure is the
+    # most import one.
+    if len(most_important_substructs) == 1:
+        return (
+            most_important_substructs["non_masked_prediction"].iloc[0]
+            - most_important_substructs["masked_prediction"].iloc[0]
+        )
+
+    atom_ids = [id for ids in most_important_substructs["atom_ids"] for id in ids]
+    mask = XAIChem.createMask(Chem.MolFromSmiles(smiles), atom_ids)
+
+    molecule_graph = XAIChem.createDataObjectFromSmiles(smiles, np.nan)
+    masked_prediction = XAIChem.predict(molecule_graph, models, mask, device)
+
+    return group.non_masked_prediction.iloc[0] - masked_prediction
+
+
+if __name__ == "__main__":
+    # Get model architecture and configuration
+    torch_device = torch.device("cuda")
+    model, config = XAIChem.models.PreBuildModels.rgcnWuEtAll(
+        "./model_config.yaml", ["seed"], model_id=0
+    )
+
+    # Load trained models
+    paths = [
+        os.path.join(DATA_DIR, f"trained_models/rgcn_model_{i}_early_stop.pt")
+        for i in range(10)
+    ]
+    rgcn_models = XAIChem.loadModels(model, paths, device="cuda")
+
+    test_attributions = pd.read_json(os.path.join(DATA_DIR, "test_attributions.json"))
+
+    print("Fidelity positive")
+    fidelities_pos = {
+        attribution_method: test_attributions.groupby("molecule_smiles")
+        .apply(
+            fidelity,
+            attribution_method,
+            models=rgcn_models,
+            device=torch_device,
+        )
+        .abs()
+        for attribution_method in ["SME", "Shapley_value", "HN_value"]
+    }
+
+    for attribution_method, fidelity_distr in fidelities_pos.items():
+        print(f"{attribution_method}: {np.round(np.mean(fidelity_distr), 2)}")
+
+    print("\nFidelity negative")
+    fidelities_neg = {
+        attribution_method: test_attributions.groupby("molecule_smiles")
+        .apply(
+            fidelity,
+            attribution_method,
+            models=rgcn_models,
+            device=torch_device,
+            mode="min",
+        )
+        .abs()
+        for attribution_method in ["SME", "Shapley_value", "HN_value"]
+    }
+
+    for attribution_method, fidelity_distr in fidelities_neg.items():
+        print(f"{attribution_method}: {np.round(np.mean(fidelity_distr), 2)}")
+
+    # app = Dash()
+
+    # figs_div = []
+
+    # for attribution_method, fidelity_distr in fidelities.items():
+    #     fig = px.histogram(x=fidelity_distr)
+    #     fig.update_layout(
+    #         autosize=False, width=800, height=500, template="plotly_white"
+    #     )
+
+    #     figs_div.append(html.Div([html.H2(attribution_method), dcc.Graph(figure=fig)]))
+
+    # app.layout = html.Div([*figs_div], style={"display": "flex"})
+    # app.run()
--- code/scripts/esol/manual_rank_vs_attribution_rank.py
+++ code/scripts/esol/manual_rank_vs_attribution_rank.py
@@ -1,3 +1,4 @@
+import os
 import re
 from ast import literal_eval
 from collections import defaultdict
@@ -6,6 +7,8 @@ from itertools import zip_longest
 import numpy as np
 import pandas as pd
 
+DATA_DIR = "../../../data/ESOL"
+
 if __name__ == "__main__":
     #############################
     # Manual rank preprocessing #
@@ -13,7 +16,7 @@ if __name__ == "__main__":
 
     # Load data and convert it to a pandas data frame
     with open(
-        "../../../data/ESOL/manual_rank_labeling.txt", "r", encoding="utf-8"
+        os.path.join(DATA_DIR, "manual_rank_labeling.txt"), "r", encoding="utf-8"
     ) as f:
         manual_ranks = f.read()
 
@@ -46,71 +49,15 @@ if __name__ == "__main__":
     manual_ranks = pd.DataFrame.from_dict(data)
     print(f"Number of molecules: {len(manual_ranks.molecule_smiles.unique())}")
 
-    #######################################
-    # Test set attributions preprocessing #
-    #######################################
+    ######################
+    # Read test set data #
+    ######################
+    data = pd.read_json(os.path.join(DATA_DIR, "test_absolute_error.json"))
+    test_attributions = pd.read_json(os.path.join(DATA_DIR, "test_attributions.json"))
 
-    data = pd.read_csv("../../../data/ESOL/ESOL_test.csv")[["smiles", "ESOL"]]
-    attributions_fg = pd.read_json(
-        "../../../data/ESOL/attribution_functional_groups.json"
-    )
-    attributions_brics = pd.read_json("../../../data/ESOL/attribution_brics.json")
-
-    # Compute for each molecule the number of substructures obtained from functional
-    # groups and BRICS and create column 'group' indicating which method create the
-    # most substructures
-    indicator_df = (
-        attributions_fg.groupby("molecule_smiles")
-        .size()
-        .to_frame("N_fg")
-        .join(attributions_brics.groupby("molecule_smiles").size().to_frame("N_brics"))
-        .apply(lambda row: "fg" if row.N_fg > row.N_brics else "brics", axis=1)
-        .to_frame("group")
-    )
-
-    # Get the attributions from the method that generates the most substructers.
-    # Subsequently filter the molecules of the test set.
-    test_attributions = (
-        pd.concat(
-            [
-                indicator_df.query("group == 'fg'").join(
-                    attributions_fg.set_index("molecule_smiles")
-                ),
-                indicator_df.query("group == 'brics'").join(
-                    attributions_brics.set_index("molecule_smiles")
-                ),
-            ]
-        )
-        .join(data.set_index("smiles"), how="right")
-        .reset_index()
-    )
-
-    # Display how many molecules cannot be split into substructures. These molecules
-    # are removed since their rank correlation will be one no matter their prediction
-    # since the rank of one number is always one.
-    smiles_no_substructures = (
-        test_attributions.groupby("molecule_smiles")
-        .size()
-        .to_frame("N")
-        .query("N == 1")
-        .index
-    )
-
-    test_attributions = test_attributions.query(
-        "molecule_smiles not in @smiles_no_substructures"
-    )
-
-    # Compute absolute error
-    data = (
-        test_attributions[["molecule_smiles", "non_masked_prediction", "ESOL"]]
-        .drop_duplicates()
-        .rename(
-            columns={"non_masked_prediction": "prediction", "molecule_smiles": "smiles"}
-        )
-    )
-
-    data["absolute_error"] = np.abs(data["prediction"] - data["ESOL"])
-
+    #############################
+    # Spearman rank correlation #
+    #############################
     # Compute Spearman Rank correlation between attributions and chemical
     # intuitive ranks
     test_attributions["atom_ids"] = test_attributions.atom_ids.apply(tuple)
@@ -141,7 +88,7 @@ if __name__ == "__main__":
         lambda value: "< 0.6" if value < 0.6 else ">= 0.6"
     )
 
-    print(data.groupby("absolute_error_class").size())
+    print(data.absolute_error_class.value_counts())
 
     print(
         data.join(
